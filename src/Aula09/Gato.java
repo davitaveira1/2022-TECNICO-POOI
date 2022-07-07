23169e6d5d5b0b53202f6c515de673506b908d9b
Davi Taveira <davitaveira@gmail.com>
2022-07-07 15:08:31 -0300

    aula 09

---
+++ src/Aula09/Gato.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula09;
+
+/**
+ *
+ * @author 2201338
+ */
+public class Gato {
+    
+    //atributos
+    
+    //metodos
+    
+    //método principal
+    //psvm
+    
+}
