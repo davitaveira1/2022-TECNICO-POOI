d14bfe661a6995f1fbcb105e918976143d5ddc37
Davi Taveira <davitaveira@gmail.com>
2022-07-07 15:42:32 -0300

    aula 09

--- src/Aula09/Gato.java
+++ src/Aula09/Gato.java
@@ -11,10 +11,61 @@ package Aula09;
 public class Gato {
     
     //atributos
+    String nome;
+    int idade;
+    String proprietario;
+    String sexo;
     
     //metodos
+    void pular(){
+        System.out.println("Gato pulando!");
+    }
     
-    //método principal
-    //psvm
+    void som(){
+        System.out.println("Miau!");
+    }
+    
+    void xixi(){
+        System.out.println("Gato fazendo xixi!");
+    }
+    
+    //apresentar as informações do gato
+    void apresentarInformacoesGato(int anoNascimento){
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("Proprietário: "+proprietario);
+        System.out.println("Sexo: "+sexo);
+        System.out.println("Ano nascimento: "+anoNascimento);
+    }
+    
+    //método principal        
+    public static void main(String[] args) {
+        Gato cat = new Gato();
+        
+        //inicializar atributos
+        cat.nome="Garfield";
+        cat.idade=6;
+        cat.proprietario="Jony";
+        cat.sexo="M";
+        
+        //chamar os métodos
+        /*
+        cat.pular();
+        cat.som();
+        cat.xixi();
+        */
+        //apresentando os atributos do gato
+        /*
+        System.out.println("Nome: "+cat.nome);
+        System.out.println("Idade: "+cat.idade);
+        System.out.println("Proprietário: "+cat.proprietario);
+        System.out.println("Sexo: "+cat.sexo);
+        */
+        //apresentando as informações via método
+        cat.apresentarInformacoesGato(2015);
+        
+        //passando informação via PARÂMETRO
+        
+    }
     
 }
